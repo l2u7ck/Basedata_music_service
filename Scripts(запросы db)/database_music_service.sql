0d95ef008263f894183d4bfa391fb49ba7c8f69a
anton <l2u7ck@gmail.com>
2024-06-20 02:03:03 +0300

    End work

--- Scripts(запросы db)/database_music_service.sql
+++ Scripts(запросы db)/database_music_service.sql
@@ -14,10 +14,13 @@ CREATE TABLE IF NOT EXISTS genresperformers (
 	CONSTRAINT pk1 PRIMARY KEY (genre_id, performer_id) 
 );
 
+--ALTER TABLE albums ADD CHECK (year_of_release >= DATE('1990-01-01') AND year_of_release <= DATE(NOW()));
+
 CREATE TABLE IF NOT EXISTS albums (
 	album_id SERIAL PRIMARY KEY,
 	title VARCHAR(100) NOT NULL,
-	year_of_release DATE NOT NULL
+	year_of_release DATE NOT NULL,
+	CHECK (year_of_release >= DATE('1990-01-01') AND year_of_release <= DATE(NOW()))	
 );
 
 CREATE TABLE IF NOT EXISTS albumsperformers (
@@ -26,17 +29,23 @@ CREATE TABLE IF NOT EXISTS albumsperformers (
 	CONSTRAINT pk2 PRIMARY KEY (album_id, performer_id) 
 );
 
+--ALTER TABLE tracks ADD CHECK (duration >= INTERVAL '20' SECOND AND duration <= INTERVAL '1' HOUR);
+
 CREATE TABLE IF NOT EXISTS tracks (
 	track_id SERIAL PRIMARY key ,
 	title VARCHAR(100) NOT NULL,
-	duration interval NOT NULL,  
+	duration INTERVAL  NOT NULL,
+	CHECK (duration >= INTERVAL '20' SECOND AND duration <= INTERVAL '1' HOUR),
 	album_id INTEGER NOT NULL REFERENCES albums(album_id) 
 );
+
+--ALTER TABLE collections ADD CHECK (year_of_release >= DATE('1990-01-01') AND year_of_release <= DATE(NOW()));
 
 CREATE TABLE IF NOT EXISTS collections (
 	collection_id SERIAL PRIMARY KEY,
 	title VARCHAR(100) NOT NULL,  
-	year_of_release DATE NOT NULL
+	year_of_release DATE NOT NULL,
+    CHECK (year_of_release > DATE('1990-01-01') AND year_of_release <= DATE(NOW()))
 );
 
 CREATE TABLE IF NOT EXISTS trackscollections (
