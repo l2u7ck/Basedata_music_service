c1ef90313f331e37400d1f94ef4f00a940d8ed0f
anton <l2u7ck@gmail.com>
2024-07-05 22:10:26 +0300

    ADD Select-Requst-3

--- Scripts(запросы db)/database_music_service_select(2,3).sql
+++ Scripts(запросы db)/database_music_service_select(2,3).sql
@@ -22,3 +22,39 @@ WHERE nickname LIKE ('% %');
 SELECT title 
 FROM tracks 
 WHERE (title LIKE ('my %')) OR (title LIKE ('% my')) OR (title LIKE ('% my %'));
+
+-- Задание 3 пункт 1 сокращенно 3.1
+SELECT g2.title,  count(performer_id) 
+FROM genresperformers g
+JOIN genres g2 ON g.genre_id = g2.genre_id 
+GROUP BY g.genre_id, g2.title 
+ORDER BY g.genre_id 
+
+-- 3.2
+SELECT COUNT(track_id)
+FROM albums a 
+JOIN tracks t ON a.album_id = t.album_id 
+WHERE year_of_release BETWEEN '2019-01-01' AND '2020-12-31' 
+
+-- 3.3
+SELECT a.title, AVG(duration) 
+FROM albums a 
+JOIN tracks t ON a.album_id = t.album_id 
+GROUP BY a.title 
+
+-- 3.4
+SELECT p.nickname 
+FROM albums a 
+JOIN albumsperformers a2 ON a.album_id = a2.album_id 
+JOIN performers p ON a2.album_id = p.performer_id 
+WHERE year_of_release NOT BETWEEN '2020-01-01' AND '2020-12-31' 
+
+-- 3.5 Исполнитель BONES
+SELECT c.title 
+FROM collections c 
+JOIN trackscollections t ON c.collection_id = t.collection_id
+JOIN tracks t2 ON t2.track_id = t.track_id 
+JOIN albums a ON a.album_id = t2.album_id 
+JOIN albumsperformers a2 ON a.album_id = a2.album_id 
+JOIN performers p ON a2.performer_id = p.performer_id 
+WHERE p.nickname = 'BONES'
